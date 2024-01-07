3816c51ce97166e77f1f8c0fa6a770586f7faf39
FatimaNaeem4144 <fatimanaeem969@gmail.com>
2024-01-07 11:34:14 +0500

    done

---
+++ model/user.js
@@ -0,0 +1,14 @@
+// Filename - User.js (or wherever your User model is defined)
+
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+// Add passport-local-mongoose as a plugin to your schema
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
